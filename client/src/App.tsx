914b2b1ff14ffd5c0d372bdfd548a388fed930c9
Komkrit-Bank <133026403+Komkrit-Bank@users.noreply.github.com>
2023-06-17 20:53:37 +0700

    create client and server side

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        <h1>MERN STACK | Workshop</h1>
+        <button className='btn btn-primary'>CLICK ME</button>
+      </div>
+    </>
+  )
+}
+
+export default App
